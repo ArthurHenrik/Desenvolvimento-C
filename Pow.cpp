f0ad1480ec10c61e7896092a6b4a0474dab3511b
ArthurHenrik <49419132+ArthurHenrik@users.noreply.github.com>
2019-04-19 15:09:11 -0300

    Add files via upload

---
+++ Pow.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int base,expoente,potencia;
+    cin>>base;
+    
+    cin>>expoente;
+    
+    potencia = pow(base,expoente);
+
+    cout<<potencia <<"\n";
+}  
