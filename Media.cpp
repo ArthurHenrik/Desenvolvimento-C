04174abd89c9aa07d734de1f0a232fa0f28e2ccc
ArthurHenrik <49419132+ArthurHenrik@users.noreply.github.com>
2019-04-19 13:04:18 -0300

    Add files via upload

--- Media.cpp
+++ Media.cpp
@@ -6,11 +6,11 @@ int main (void){
     int gordo[10];
 
     
-    for(int i=1;i<=10; i++){
+    for(int i=0;i<=10; i++){
        cin>>gordo[i];
        
     }
-    for(int i=9;i>=1;i--){
+    for(int i=9;i>=0;i--){
          cout<<gordo[i]<<"\n";
     }
 }
